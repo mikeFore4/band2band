304a61fc493dfda1e49d8f2f20a90fa2f3e37448
mikeFore4 <michaelfore4@gmail.com>
2021-03-09 02:51:48 -0500

    Adding script and new dataset to preprocess filenames

---
+++ build_dataset.py
@@ -0,0 +1,40 @@
+import os
+from itertools import permutations
+import argparse
+import csv
+
+def create_pair_csv(data_dir, output_path):
+    print(data_dir)
+    print(output_path)
+    #extract all directories for common image filename stems
+    image_groups = [os.path.join(data_dir,x) for x in os.listdir(data_dir)]
+
+    #extract every tile directory
+    tile_dirs = []
+    for ig in image_groups:
+        tile_dirs.extend([os.path.join(ig,x) for x in os.listdir(ig)])
+
+    #get all pairwise combinations of tiles of the same image
+    pairs = []
+    with open(output_path, 'w') as f:
+        writer = csv.writer(f)
+        for d in tile_dirs:
+            fns = [os.path.join(d,x) for x in os.listdir(d)]
+            perms = permutations(fns,2)
+            for p in perms:
+                writer.writerow(p)
+
+def process_dir(data_dir):
+    subsets = os.listdir(data_dir)
+
+    for sub in subsets:
+        create_pair_csv(os.path.join(data_dir, sub),
+                os.path.join(data_dir,f'{sub}_pairs.csv'))
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data-dir',type=str)
+
+    args = parser.parse_args()
+
+    process_dir(**vars(args))
